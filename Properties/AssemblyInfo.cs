1359008d2b6f0f40d320caed14db3af3b1cfd6ad
subbob <subbob@gmail.com>
2022-07-13 11:03:33 -0500

    Update AssemblyInfo.cs
    Changed ChatFilter to ChatAliases

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ChatFilter")]
+[assembly: AssemblyTitle("ChatAliases")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ChatFilter")]
+[assembly: AssemblyProduct("ChatAliases")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
